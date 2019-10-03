7d27285dd2e83c67bc6e222ef520f4abbdc1e58b
micgogi <rahul.gogyani@gmail.com>
2019-10-03 22:53:15 +0530

    added

---
+++ src/main/java/com/micgogi/awsspringapp/AwsspringappApplication.java
@@ -0,0 +1,13 @@
+package com.micgogi.awsspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsspringappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsspringappApplication.class, args);
+	}
+
+}
